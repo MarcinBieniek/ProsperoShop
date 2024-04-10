7d7dde740db3fc10aaa1ac9822af834d2b398b33
Marcin Bieniek <marcingim@gmail.com>
2024-04-10 16:51:32 +0200

    PS-6 - feat: add get user listings api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,12 +1,15 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
+import Listing from '../models/listing.model.js';
 
 export const test = (req, res) => {
   res.json({
     message: "User api route is working",
   })
 }
+
+// Update user data
 
 export const updateUser = async (req, res, next) => {
   if(req.user.id !== req.params.id) return next(errorHandler(401, "You can only update your own account!"))
@@ -43,5 +46,19 @@ export const deleteUser = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
+}
 
+// Get user listings
+
+export const getUserListings = async (req, res, next) => {
+  if(req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listing'));
+  }
 }
