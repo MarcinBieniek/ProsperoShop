ce2b7681aa6975f29563df0a5250e5a5d7435dbc
Marcin Bieniek <marcingim@gmail.com>
2024-04-11 13:30:42 +0200

    PS-6 - feat: add delete listing feat

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,12 +1,31 @@
 import Listing from "../models/listing.model.js"
 
+// Create listing
 export const createListing = async (req, res, next) => {
-
   try {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing)
   } catch (error) {
     next(error)
   }
-
 };
+
+// Delete listing
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorhandler(401, 'You can only delete your own listings!'))
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json('Listing has been deleted!');
+  } catch (error) {
+    next(error);
+  }
+}
