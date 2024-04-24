2d66b135cb48c739bc299f414db11accf814b741
Marcin Bieniek <marcingim@gmail.com>
2024-04-24 23:41:25 +0200

    PS-7 - feat: add full cart dropdown menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,13 +9,15 @@ import { FaRegUser } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import { HiOutlineWrenchScrewdriver } from "react-icons/hi2";
 import { PiGarage } from "react-icons/pi";
+import { LuPackage } from "react-icons/lu";
+import { TiDeleteOutline } from "react-icons/ti";
 
 const Header = () => {
 
   const { currentUser } = useSelector(state => state.user)
 
-  const [isCart, setIsCart] = useState(true);
-  const [isCartHovered, setIsCartHovered] = useState(false);
+  const [isCart, setIsCart] = useState(false);
+  const [isCartHovered, setIsCartHovered] = useState(true);
 
   return (
 
@@ -50,7 +52,7 @@ const Header = () => {
                 referrerPolicy="no-referrer"
               />
             ) : (
-              <div className=' bg-neutral-900 flex items-center gap-2 py-2 px-3 hover:bg-white hover:text-neutral-900 transition-colors transition-text duration-300 ease-in-out '>
+              <div className=' bg-neutral-900 flex items-center gap-2 py-2 px-3 hover:bg-white hover:text-neutral-900 transition-colors transition-text duration-300 ease-in-out rounded'>
                 <FaRegUser className='text-xs' />
                 <p className='uppercase text-xs'>Zaloguj</p>
               </div>
@@ -60,8 +62,16 @@ const Header = () => {
 
         <div
           className='basket ml-10 mr-20 w-36 flex items-center gap-2 py-2 px-3 relative'
+          /*
+
           onMouseEnter={() => setIsCartHovered(true)}
           onMouseLeave={() => setIsCartHovered(false)}
+
+          do poprawnego wyświetlania:
+          - zmienić opacity na 0 w description
+          - zmienić state na false
+
+          */
         >
           <div className='flex items-center justify-center relative'>
             <IoCartOutline className='text-lg' />
@@ -72,12 +82,13 @@ const Header = () => {
           <p className='uppercase text-xs'>Twój koszyk</p>
           {isCartHovered ? (
           <>
-            <div className='description absolute top-8 right-12 w-0 h-0 border-l-[20px] border-l-transparent border-b-[25px] border-b-white border-r-[20px] border-r-transparent opacity-0 transition-opacity duration-300'></div>
-            <div className='description bg-white absolute top-[57px] right-5 w-80 text-neutral-900 opacity-0 transition-opacity duration-300 p-3'>
-              <div className='font-bold text-white flex justify-center mx-auto bg-orange-600 p-2'>
+            <div className='description absolute top-8 right-12 w-0 h-0 border-l-[20px] border-l-transparent border-b-[25px] border-b-white border-r-[20px] border-r-transparent opacity-1 transition-opacity duration-300'></div>
+            <div className='description bg-white absolute top-[57px] right-5 w-80 text-neutral-900 opacity-1 transition-opacity duration-300 p-3 rounded'>
+              <div className='font-bold text-white flex justify-center mx-auto bg-orange-600 p-2 rounded'>
                 <p>Koszyk</p>
               </div>
               <div className='flex-col justify-center'>
+
                 {isCart ? (
                   <div>
                     <div>
@@ -104,8 +115,62 @@ const Header = () => {
                     </div>
                   </div>
                 ) : (
-                  <div></div>
+                  <div>
+
+                    <Link to='/'>
+                      <div className='flex py-2 gap-2 hover:bg-gray-100 rounded my-2'>
+                        <img
+                          src="/produkt.jpg"
+                          alt='Product image'
+                          className='w-24 object-cover'
+                          referrerPolicy="no-referrer"
+                        />
+                        <div>
+                          <p className='font-bold text-sm mb-1'>Brama przesuwna Modern</p>
+                          <p className='text-xs mb-1'>Wymiar: 350X145, kolor: RAL7016, automatyka: AWso2018</p>
+                          <div className='flex text-sm justify-between items-center'>
+                            <TiDeleteOutline className='text-red-600'/>
+                            <p>Ilość: 1</p>
+                            <p>Cena: 6500 zł</p>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+
+                    <Link to='/'>
+                      <div className='flex py-2 gap-2 hover:bg-gray-100 rounded my-2'>
+                        <img
+                          src="/produkt.jpg"
+                          alt='Product image'
+                          className='w-24 object-cover'
+                          referrerPolicy="no-referrer"
+                        />
+                        <div>
+                          <p className='font-bold text-sm mb-1'>Brama przesuwna Modern</p>
+                          <p className='text-xs mb-1'>Wymiar: 350X145, kolor: RAL7016, automatyka: AWso2018</p>
+                          <div className='flex text-sm justify-between items-center'>
+                            <TiDeleteOutline className='text-red-600'/>
+                            <p>Ilość: 1</p>
+                            <p>Cena: 6500 zł</p>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+
+                    <div className='flex flex-col items-end border-t border-orange-600 py-3'>
+                      <p className='text-orange-600 font-bold pb-1'>Suma: 6500 zł brutto</p>
+                      <p className='text-green-700 font-bold'>Transport gratis!</p>
+                    </div>
+
+                    <div className='flex justify-center border-t border-orange-600 pt-3'>
+                      <Link to='/kontakt' className='bg-neutral-900 py-2 px-3 text-white hover:bg-orange-600 transition-text duration-200 flex items-center gap-1 rounded'>
+                        <LuPackage />
+                        <p className='text-xs uppercase'>Zamów</p>
+                      </Link>
+                    </div>
+                  </div>
                 )}
+
               </div>
             </div>
             </>
